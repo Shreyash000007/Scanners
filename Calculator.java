ab929d06563e2f168fb68d59d9cbc037f4e012b4
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-23 15:04:49 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator{
+// prepare function with two paramaters and name it as add and return addition of two paramaters as integer
+// prepare function with two paramaters and name it as sub and return Multiplication of two paramaters as integer
+// prepare function with two paramaters and name it as multi and return Substraction of two paramaters as integer
+// prepare function with two paramaters and name it as division and return Division of two paramaters as integer
+  public static void main(String[]args){
+  // Call that functions
+  } 
+}
