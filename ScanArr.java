3e6b4218db8813b880003d5331084eb596bd57eb
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-23 15:08:11 +0530

    Create ScanArr.java

---
+++ ScanArr.java
@@ -0,0 +1,7 @@
+public class ScanArr{
+// prepare a function with return type int[]
+  public static void main(String[]args){
+// Call the Function here and initialize an Array using scanner
+    
+  }
+}
