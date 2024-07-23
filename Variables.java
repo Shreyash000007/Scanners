8e3793f499b0bad45135c05811aef09cc403c6df
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-23 14:48:25 +0530

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,10 @@
+public class Varibles{
+  // Take each Data Type for Each Variable and Initialize them using scanner here
+  // id ->int
+  // name ->String
+  // sal ->double
+  // gender ->char
+  // Pass ->boolean
+public static void main(String[]args){
+// Call the varibles in the main method and print them
+}}
