a5c5fe0d7e65f3d8db5121be19b471df40b5aaee
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-23 14:51:29 +0530

    Create BugFix.java

---
+++ BugFix.java
@@ -0,0 +1,9 @@
+public class BugFix{
+Scanner in=new Scanner(System.in);
+  int id;
+  public static void main(String[]args){
+    BugFix b=new BugFix();
+    id=in.nextInt();
+    System.out.println()
+}
+}
