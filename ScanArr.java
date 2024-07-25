dcd8bbbd37296fa199e75096ec240c9b21840417
Shreyash000007 <101011157+Shreyash000007@users.noreply.github.com>
2024-07-25 16:20:37 +0530

    Update ScanArr.java

--- ScanArr.java
+++ ScanArr.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 public class ScanArr{
 // prepare a function with return type int[]
+public int[] method1()
+{
+  Scanner aobj=new Scanner(System.in);
+  System.out.println("-----------------------------------------------");
+  System.out.print("Enter the Element = ");
+  int abc=aobj.nextInt();
+  int[] array=new int[abc];
+  System.out.println("Enter the element = ");
+  for(int i=0;i<array.length;i++)
+  {
+    array[i]=aobj.nextInt();
+  }
+  return array;
+}
   public static void main(String[]args){
 // Call the Function here and initialize an Array using scanner
-    
+    ScanArr aobj2=new ScanArr();
+    int[] array=aobj2.method1();
+    System.out.println("Enter Element = ");
+    for (int i : array) 
+    {
+      System.out.println(i);
+    }
   }
 }
