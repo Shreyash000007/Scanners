32ccc7a37791d4caa1dc9f50e09a4be6e6320d6e
Shreyash000007 <101011157+Shreyash000007@users.noreply.github.com>
2024-07-25 16:19:11 +0530

    Update BugFix.java

--- BugFix.java
+++ BugFix.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
 public class BugFix{
 Scanner in=new Scanner(System.in);
   int id;
   public static void main(String[]args){
     BugFix b=new BugFix();
-    id=in.nextInt();
-    System.out.println()
+    System.out.println("Enter your id = ");
+   int id=b.in.nextInt();
+    System.out.println("You Entered Id = "+id);
 }
 }
